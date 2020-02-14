7b14b8038bbded3c7aa6fad4ddcd54b191595f36
Andre Nunes <a.sousa@runtime-revolution.com>
2020-02-14 09:38:43 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import IndexScreen from './src/screens/IndexScreen'
+
+
+const navigator = createStackNavigator({
+  Index: IndexScreen
+  }, {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />
+}
